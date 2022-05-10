6fec5acb81dcdc6cc6e0318a0e708c80f0efa81b
iamaravindraj <arvindraj4010@gmail.com>
2022-05-10 12:53:27 +0530

    1st commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld{
 	public static void main(String [] args){
 	System.out.println("Hello World");
 	String name = "Aravind"
-	System.out.println(name);	
+	System.out.println("Adding more features");
  }
 }
